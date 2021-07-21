c20223d00e8e25002fa8dcb3664b71306e538c18
RyanVohra <87702860+RyanVohra@users.noreply.github.com>
2021-07-21 16:21:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var box
+
+function setup(){
+
+
+    {
+    if(keyIsDown(DOWN_ARROW))
+background('red');
+
+    }
+
+
+}
+box.createSprite(200,200,20,20)
+
+
+function draw(){
+drawSprites()
+}
+
